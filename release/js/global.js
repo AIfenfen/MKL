ab72cf1283418084c6a8b54972a273c447fb8b3f
wangx <wangx@ickey.cn>
2016-08-23 16:38:30 +0800

    fixed
    index setting

--- release/js/global.js
+++ release/js/global.js
@@ -37,52 +37,8 @@ var Common = {
         }
         return true;
     },
-    email : function(obj){
-        var objVal = $.trim(obj.val()),
-            strReg = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        if(!strReg.test(objVal)){
-            Common.showProp(Common.errorMsg(obj));
-            return false;
-        }
-        return true;
-    },
-    qq : function(obj){
-        var objVal = $.trim(obj.val()),
-            strReg = /^\d{6,}$/;
-        if(!strReg.test(objVal)){
-            Common.showProp(Common.errorMsg(obj));
-            return false;
-        }
-        return true;
-    },
-    account : function(obj){
-        var objVal = $.trim(obj.val()),
-            strReg = /^[a-zA-Z]\w{5,15}$/;
-        if(!strReg.test(objVal)){
-            Common.showProp(Common.errorMsg(obj));
-            return false;
-        }
-        return true;
-    },
-    pwd : function(obj){
-        var objVal = $.trim(obj.val()),
-            strReg = /^[A-Za-z0-9]{6,16}$/;
 
-        if(!strReg.test(objVal)){
-            Common.showProp(Common.errorMsg(obj));
-            return false;
-        }
-        return true;
-    },
-    repwd : function(obj,pwd){
-        var objVal = $.trim(obj.val()),
-            pwd = $.trim(pwd);
-        if(objVal!=pwd){
-            Common.showProp(Common.errorMsg(obj));
-            return false;
-        }
-        return true;
-    },
+
     ajaxFileUpload : function(obj){
         var closestBox = obj.closest("div"),
             idname = obj.attr("id"),
@@ -144,10 +100,10 @@ var Common = {
                         Common.showProp(result.msg,Message.m009);
                         setTimeout(function(){window.location.href=result.url},3000); //这个url指的需要跳转到的页面，例如登入成功跳到首页，找回密码成功就可能跳到个人中心。
                     }else{
-                        Common.showProp(result.msg);      //msg为返回状态，这里是例如登入成功，或者注册成功
+                        Common.showProp(result.msg);  //msg为返回状态，这里是例如登入成功，或者注册成功
                     }
-                }else if(type == 0){    //0、失败
-                    Common.showProp(result.msg);     //mgs为返回状态，这里是失败的原因。
+                }else if(type == 0){  //0、失败
+                    Common.showProp(result.msg);  //mgs为返回状态，这里是失败的原因。
                     if(typeof(options.fn) != "undefined"){
                         options.fn();
                     }
@@ -167,93 +123,6 @@ var Common = {
                 }
                 options.btn.removeAttr("disabled");
             }
-        });
-    },
-    loadMore : function(options){
-        var options = $.extend({
-            url:"",         //参数
-            data:"",        //动态操作数据
-            fn: "",         //伪回调
-            type:"post",    //提交方式，默认post
-            dataType:"json",    // 回调数据类型 json
-            box:"",             //列表盒子
-            page:2,             //起始页
-            loadbox : "<div class='loadingBox'></div>"  //加载动画
-        },options);
-        $(window).scroll(function(){
-            var scrollTop = $(this).scrollTop();
-            var scrollHeight = $(document).height();
-            var windowHeight = $(this).height();
-            if(scrollTop + windowHeight >= scrollHeight){   //到达底部
-                var loading = $(".loadingBox");
-                if(loading.length&&loading.is(":visible")) return;      //禁止重复加载
-                !loading.length?$("footer").before(options.loadbox):loading.show();
-                link_url = options.url.replace('{page}',options.page++); //设置请求url
-                $.ajax({
-                    url : link_url,
-                    data : options.date,
-                    type : options.type,
-                    dataType : options.dataType,
-                    success : function(result){
-                        var type = result.type;// type 为返回状态
-                        if(type == 1){      //1、成功
-                            if(options.fn==""){
-                                options.box.append(result.msg);
-                            }else{
-                                options.fn(options,result.msg);
-                            }
-
-                        }else{  //2、失败
-                            Common.showProp(Message.m008);   //请求超时，请重试
-                        }
-                        $(".loadingBox").hide();
-                    },
-                    error:function(){
-                        Common.showProp(Message.m008);   //请求超时，请重试
-                        loading.hide();
-                    }
-                });
-            }
-         });
-    },
-    changeDate : function(options){
-        var options = $.extend({
-            url:"",         //参数
-            data:"",        //动态操作数据
-            fn: "",         //伪回调
-            type:"post",    //提交方式，默认post
-            dataType:"json",    // 回调数据类型 json
-            box:"",             //列表盒子
-            btn:""
-        },options);
-        options.btn.click(function(){
-            var _this = $(this);
-            _this.attr("disabled","disabled");
-            options.box.addClass("asynloding");
-            $.ajax({
-                url : options.url,
-                data : options.date,
-                type : options.type,
-                dataType : options.dataType,
-                success : function(result){
-                    var type = result.type;// type 为返回状态
-                    if(type == 1){      //1、成功
-                        if(options.fn==""){
-                            options.box.html(result.msg).removeClass("asynloding");
-                        }else{
-                            options.fn(options,result.msg);
-                        }
-                    }else{  //2、失败
-                        Common.showProp(Message.m008);   //请求超时，请重试
-                    }
-                    _this.removeAttr("disabled");
-                },
-                error:function(){
-                    Common.showProp(Message.m008);   //请求超时，请重试
-                    options.box.removeClass("asynloding");
-                    _this.removeAttr("disabled");
-                }
-            });
         });
     }
 }
@@ -292,28 +161,6 @@ var Common = {
                 });
             }
             loadrun = setInterval(loadData,options.rate);
-        },
-        tabs:function(options){
-            var options = $.extend({
-                event:'click',
-                classname:'on'
-            },options);
-            var self = $(this);
-            self.each(function(i){
-                var _this = $(this);
-                if(_this.find("a").length)return;
-                _this.bind(options.event,function(){
-                    _this.addClass(options.classname).siblings().removeClass(options.classname);
-                    options.els.hide();
-                    options.els.eq(i).show();
-                    return false;
-                });
-            });
-
         }
     });
-})(jQuery);
-
-var Message = {
-
-};
+})(zepto);
